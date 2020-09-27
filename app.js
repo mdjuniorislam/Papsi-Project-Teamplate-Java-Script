a44613387432ae56c38d4ab9c647248a63de65cb
mdjuniorislam <70217801+mdjuniorislam@users.noreply.github.com>
2020-09-27 19:54:17 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+function show_papsifst()
+{
+
+    document.getElementById('papsi-img').src ="./image/pepsi001.png";
+      document.getElementsByClassName('main')[0].style.backgroundColor = "red";
+
+}
+function show_papsisec()
+{
+
+    document.getElementById('papsi-img').src ="./image/pepsi002.png";
+      document.getElementsByClassName('main')[0].style.backgroundColor = "#050721";
+
+}
+
+function show_papsitrd()
+{
+
+    document.getElementById('papsi-img').src ="./image/pepsi002.png";
+      document.getElementsByClassName('main')[0].style.backgroundColor = "#1F10EB";
+
+}
